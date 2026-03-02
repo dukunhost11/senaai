6f39b7d126eb92f5b41fcdba9a0d3e4c9bd3fde2
dukunhost11 <152787968+dukunhost11@users.noreply.github.com>
2026-03-02 11:16:58 +0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,6 @@
+var button = document .getElementById("changeCollorButton");
+
+button .addEventListener("click", function() {
+var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+document.body.style.backgroundColor = randomColor;
+});
